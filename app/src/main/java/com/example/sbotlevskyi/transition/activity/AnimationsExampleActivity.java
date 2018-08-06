f36b80bc01eba251681e231c4aa87d05102c3df6
Sergii Botlevskyi <SBotlevskyi@on.seo>
2018-08-06 10:59:17 +0300

    add button

--- app/src/main/java/com/example/sbotlevskyi/transition/activity/AnimationsExampleActivity.java
+++ app/src/main/java/com/example/sbotlevskyi/transition/activity/AnimationsExampleActivity.java
@@ -1,6 +1,5 @@
 package com.example.sbotlevskyi.transition.activity;
 
-import android.animation.ValueAnimator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,6 +13,8 @@ public class AnimationsExampleActivity extends AppCompatActivity {
     private static final String TAG = "AnimationsExample";
     private Animation animation;
     private Button button1;
+    private Button button2;
+    private Button button3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,16 +22,32 @@ public class AnimationsExampleActivity extends AppCompatActivity {
         setContentView(R.layout.activity_animations_example);
 
         button1 = findViewById(R.id.button1);
-        animation = AnimationUtils.loadAnimation(this, R.anim.transition_button1);
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
+
+        animation = AnimationUtils.loadAnimation(this, R.anim.translation_x_button);
         button1.startAnimation(animation);
 
-//        button1.animate()
-//                .setDuration(1000)
-//                .setUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-//                    @Override
-//                    public void onAnimationUpdate(ValueAnimator valueAnimator) {
-//                        Log.i(TAG, "onAnimationUpdate: ");
-//                    }
-//                }).start();
+        animation = AnimationUtils.loadAnimation(this, R.anim.translation_y_button);
+        button2.startAnimation(animation);
+
+
+
+        button3.setOnClickListener(view -> {
+            final int[] i = {1};
+            final int[] value = {1};
+            button3.animate()
+                    .setDuration(5000)
+                    .setUpdateListener(valueAnimator -> {
+                                i[0] += value[0];
+                                button3.setTranslationY(i[0]);
+                                if (Math.abs(i[0])>20){
+                                    value[0] *=-1;
+                                }
+                                Log.i(TAG, "onAnimationUpdate: ");
+                            }
+                    ).start();
+        });
+
     }
 }
